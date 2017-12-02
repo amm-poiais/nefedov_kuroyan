5b039d12a372b7a3921f0b5d525d75bd6f9153ba
Nef1k <nef1k@outlook.com>
2017-12-02 14:55:00 +0300

    Created models to store game statistics

--- game_stats/models.py
+++ game_stats/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from authentication.models import User
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    priority = models.IntegerField()
+    image_url = models.CharField(max_length=255)
+
+
+class Difficulty(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class ScoreEntry(models.Model):
+    user = models.ForeignKey(to=User)
+    game = models.ForeignKey(to=Game)
+    difficulty = models.ForeignKey(to=Difficulty)
+    best_score = models.IntegerField()
+
+    class Meta:
+        unique_together=(('user', 'game', 'difficulty'),)
